010bc5343c8152923d9fcdabca1631a4969d92e0
zhangxifa <chinazxf@126.com>
2021-04-11 15:09:56 +0800

    创建了一个Helloworld的文件

---
+++ day01-code/src/cn/test/day01/demo01/HelloWorld.java
@@ -0,0 +1,23 @@
+package cn.test.day01.demo01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s1= "小明";
+        System.out.println(s1);
+        System.out.println("Hello World!");
+
+        System.out.println("小猫爱吃鱼");
+//        定义一个集合
+/**
+ * 定义一个集合
+ */
+
+        List list = new ArrayList();
+
+
+
+    }
+}
